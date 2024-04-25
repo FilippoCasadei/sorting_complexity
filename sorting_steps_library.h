d5f45548361f2e7e616871d5f6f9f8a1e792bbeb
FilippoCasadei <147723968+FilippoCasadei@users.noreply.github.com>
2024-04-26 01:05:08 +0200

    https://github.com/FilippoCasadei/sorting_complexity.git

---
+++ sorting_steps_library.h
@@ -0,0 +1,9 @@
+double bubble_sort_steps(int arr[], int n);
+double selection_sort_steps(int arr[], int n);
+double insertion_sort_steps(int arr[], int n);
+double merge_sort_steps(int arr[], int l, int r);
+double merge_steps(int arr[], int l, int m, int r);
+double quick_sort_steps(int arr[], int low, int high);
+int partition_steps(int arr[], int low, int high, double *steps);
+void heapify_steps(int arr[], int n, int i, double *steps);
+double heap_sort_steps(int arr[], int n);

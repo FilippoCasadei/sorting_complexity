a2b30f4b24cc8b4b81edd2e25872fb3f79dbfc0b
FilippoCasadei <147723968+FilippoCasadei@users.noreply.github.com>
2024-04-26 02:46:59 +0200

    Eliminazione file singoli

--- sorting_library.h
+++
@@ -1,9 +0,0 @@
-void bubble_sort(int a[], int n);
-void selection_sort(int a[], int n);
-void insertion_sort(int a[], int n);
-void merge_sort(int a[], int sx, int dx);
-void merge(int a[], int sx, int mx, int dx);
-void quick_sort(int a[], int sx, int dx);
-void swap(int *a, int *b);
-void heapify(int arr[], int n, int i);
-void heap_sort(int arr[], int n);
--- testing.h
+++
@@ -1,3 +0,0 @@
-// Dichiarazione delle funzioni per il testing
-
-void stampa_array(int array[], int lunghezza);
